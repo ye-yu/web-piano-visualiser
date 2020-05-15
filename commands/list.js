9a44e10e43bfa8bcc235ad2fa2c43e1d40469416
ye-yu <rafolwen98@gmail.com>
2020-05-16 06:03:58 +0800

    Add help msg and output list in list command

--- commands/list.js
+++ commands/list.js
@@ -1,11 +1,30 @@
 function call(args) {
-  console.log('Input devices:');
   var i=0;
-  require('../index').getEM().getInputs().forEach(inp => {
-    console.log(`  [${i++}] ${inp}`);
-  });
+  if (args[0] === 'input' || args[0] === undefined) {
+    console.log('Input devices:');
+    require('../index').getEM().getInputs().forEach(inp => {
+      console.log(`  [${i++}] ${inp}`);
+    });
+  } else if (args[0] === 'output') {
+    console.log('Output devices:');
+    require('../index').getEM().getOutputs().forEach(inp => {
+      console.log(`  [${i++}] ${inp}`);
+    });
+  } else {
+    console.log(`Invalid args: ${args.join(' ')}`);
+  }
+}
+
+function help() {
+  return `Usage:
+  list
+  list [input|output]
+
+List down input or output devices. Input devices will be listed
+by default if there are no args provided.`;
 }
 
 module.exports = {
-  call: call
+  call: call,
+  help: help
 };
