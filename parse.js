e65a0dedd563e2c424eef681816a41e65c424fa7
ye-yu <rafolwen98@gmail.com>
2020-05-16 08:14:00 +0800

    Include error message on parse error

--- parse.js
+++ parse.js
@@ -2,7 +2,7 @@ function parse(args) {
   try {
     require(`./commands/${args[0]}`).call(args.slice(1));
   } catch (err) {
-    console.error(`Command not recognised: ${args.join(' ')}`);
+    console.error(`Command not recognised: ${args.join(' ')}`, err);
   }
 }
 
