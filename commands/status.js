527bbfcc9f3103b1c3fb4462302591aaf32a7548
ye-yu <rafolwen98@gmail.com>
2020-05-21 10:01:06 +0800

    Add json to status

--- commands/status.js
+++ commands/status.js
@@ -6,6 +6,20 @@ function call(args, stdout) {
   const INP_STATUS = require('../index').getGlobVar('inputConnected', false) ? 'connected' : 'disconnected';
   const OUT = require('../index').getGlobVar('outputName', CHALK.yellow('UNSET'));
   const OUT_STATUS = require('../index').getGlobVar('outputConnected', false) ? 'connected' : 'disconnected';
+
+  if(args[0] == 'json') {
+    stdout.json({
+      input: {
+        name: INP,
+        status: INP_STATUS
+      },
+      output: {
+        name: OUT,
+        status: OUT_STATUS
+      }
+    });
+    return;
+  }
   stdout.info(CHALK.green('Input device:'), `${INP}`, `(${CHALK.yellow(INP_STATUS)})`);
   stdout.info(CHALK.green('Output device:'), `${OUT}`, `(${CHALK.yellow(OUT_STATUS)})`);
 }
